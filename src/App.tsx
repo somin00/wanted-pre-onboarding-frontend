77be15d3f526df52275e1223c44cecaf1247dcde
somin <smhs12@naver.com>
2023-08-01 23:21:57 +0900

    [feat] 리다이렉트 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import Home from "pages/Home";
 import Signin from "pages/Signin";
 import Signup from "pages/Signup";
 import Todo from "pages/Todo";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (!token && pathname === "/todo") {
+      navigate("/signin");
+    } else if ((token && pathname === "/signup") || (token && pathname === "/signin")) {
+      navigate("/todo");
+    }
+  }, [navigate, pathname]);
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/signin" element={<Signin />}></Route>
-          <Route path="/todo" element={<Todo />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/todo" element={<Todo />}></Route>
+      </Routes>
     </>
   );
 }
--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 function Todo() {
   return <div>투두페이지</div>;
 }
