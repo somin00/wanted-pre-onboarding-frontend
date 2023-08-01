d878e39b14934c9f60677c3b6e71345ef7fa6aa2
somin <smhs12@naver.com>
2023-08-01 18:55:51 +0900

    [style] 로그인 페이지 마크업 및 스타일 적용

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AuthForm from "components/AuthForm";
+import { SigninWrapper } from "./styles";
 
 function Signin() {
-  return <div>로그인페이지</div>;
+  return (
+    <SigninWrapper>
+      <h1>로그인 페이지</h1>
+      <AuthForm />
+      <button data-testid="signin-button">로그인</button>
+    </SigninWrapper>
+  );
 }
 
 export default Signin;
